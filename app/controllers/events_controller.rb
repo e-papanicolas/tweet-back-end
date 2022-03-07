a4196aa7aa85b10ee8f6423a3990d2f3a6ad6569
Eleni Papanicolas <elenipapanicolas@gmail.com>
2022-03-07 15:56:23 -0500

    added update route for event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -21,6 +21,13 @@ class EventsController < ApplicationController
     render json: new_event, serializer: EventSerializer, status: :created
   end
 
+  def update 
+    event = get_event
+    event.update(event_params)
+    event.save
+    render json: event, serializer: EventSerializer
+  end
+
   def destroy 
     event = get_event 
     TwitterStream.delete_single_rule(event.rule_id)
@@ -36,5 +43,9 @@ class EventsController < ApplicationController
 
   def event_params 
     params.permit(:name, :timeout, :hashtag, :rule_id, :user_id, :id) 
+  end
+
+  def update_params
+    params.permit(:name, :timeout, :hashtag)
   end
 end
